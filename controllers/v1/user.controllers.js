98bcecacfad7ff48b8f35d765aff98d339e1efe1
unknown <rajebul.alam17@gmail.com>
2022-12-01 16:30:23 +0600

    Complete operation get random user, get all user, post a user, update user with single id, update with multiple id and delete user

--- controllers/v1/user.controllers.js
+++ controllers/v1/user.controllers.js
@@ -3,8 +3,8 @@ let allData = fs.readFileSync('user.json');
 const items = JSON.parse(allData);
 let users = items;
 
+// 1.  Get Random data using nodejs file system
 module.exports.getRandomUser = (req, res) => {
-    // Get Random data using nodejs file system
     const item = items[Math.floor(Math.random() * items.length)];
     console.log(item);
     res
@@ -16,9 +16,8 @@ module.exports.getRandomUser = (req, res) => {
         });
 };
 
+// 2.  Get all data from data.json file
 module.exports.getAllUser = (req, res) => {
-    // Get all data from data.json file
-    const items = JSON.parse(allData);
     console.log(items);
     res
         .status(200)
@@ -29,8 +28,9 @@ module.exports.getAllUser = (req, res) => {
         });
 };
 
+
+// 3. Post or Save a random user
 module.exports.postARandomUser = (req, res) => {
-    // Post or Save a random user
     const user = req.body;
     console.log(user);
     users.push(user);
@@ -43,6 +43,7 @@ module.exports.postARandomUser = (req, res) => {
         });
 };
 
+// 4.  Update Random user
 module.exports.updateRandomUser = (req, res) => {
     const { id } = req.params;
     const updateUser = users.find(user => user.id === Number(id));
@@ -65,21 +66,50 @@ module.exports.updateRandomUser = (req, res) => {
         });
 };
 
+//  5. Update multiple user
 module.exports.updateMultipleUser = (req, res) => {
     const { id } = req.params;
-    console.log(id);
-    const updateUser = users.find(user => user.id === Number(id.slice));
+    // First one
+    // When we send multiple id  
+    const firstId = id.slice(0, 1);
+    const updateFirstIdUser = users.find(user => user.id === Number(firstId));
+    const response = req.body;
+    const bodyDataForFirst = response.slice(0).find(resp => resp.id);
+
+    // Update first request data
+    bodyDataForFirst.id ? updateFirstIdUser.id = bodyDataForFirst.id : '';
+    bodyDataForFirst.gender ? updateFirstIdUser.gender = bodyDataForFirst.gender : '';
+    bodyDataForFirst.name ? updateFirstIdUser.name = bodyDataForFirst.name : '';
+    bodyDataForFirst.contact ? updateFirstIdUser.contact = bodyDataForFirst.contact : '';
+    bodyDataForFirst.address ? updateFirstIdUser.address = bodyDataForFirst.address : '';
+    bodyDataForFirst.photoUrl ? updateFirstIdUser.photoUrl = bodyDataForFirst.photoUrl : '';
+    console.log(updateFirstIdUser);
+
+    // Second one
+    const secondId = id.slice(2);
+    const updateSecondIdUser = users.find(user => user.id === Number(secondId));
+    const bodyDataForSecond = response.slice(1).find(resp => resp.id);
+
+    // Update with second request data
+    bodyDataForSecond.id ? updateSecondIdUser.id = bodyDataForSecond.id : '';
+    bodyDataForSecond.gender ? updateSecondIdUser.gender = bodyDataForSecond.gender : '';
+    bodyDataForSecond.name ? updateSecondIdUser.name = bodyDataForSecond.name : '';
+    bodyDataForSecond.contact ? updateSecondIdUser.contact = bodyDataForSecond.contact : '';
+    bodyDataForSecond.address ? updateSecondIdUser.address = bodyDataForSecond.address : '';
+    bodyDataForSecond.photoUrl ? updateSecondIdUser.photoUrl = bodyDataForSecond.photoUrl : '';
+    console.log(updateSecondIdUser);
     res
-        .status(200)
-        .send({
+        .status(200).send({
             success: true,
-            message: 'User Updated',
-            data: updateUser
+            message: 'Update multiple data successfully',
+            data: users
         });
+
 };
 
+
+// 6.  Delete user with ID
 module.exports.deleteARandomUser = (req, res) => {
-    // Delete user with ID
     const { id } = req.params;
     const deleteUser = users.filter(user => user.id !== Number(id))
     console.log(deleteUser);
